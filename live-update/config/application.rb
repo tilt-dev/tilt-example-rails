a4f6abb2e3e6ab56b3d7d8b2f1981e4138d80a5e
Maia <maia.mcc@gmail.com>
2020-12-08 12:20:06 -0500

    fix config whoops

--- live-update/config/application.rb
+++ live-update/config/application.rb
@@ -11,6 +11,9 @@ module Sample
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # https://github.com/refinery/refinerycms/issues/3466 -_-
+    config.autoloader = :classic
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
